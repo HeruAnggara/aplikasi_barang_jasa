01098c992db3fdf84f79a1056ed5102f18af342a
Heru Anggara <heruanggara0909@gmail.com>
2023-10-23 10:10:06 +0700

    update tambahLaporan dto

--- src/pengajuan/dto/tambahLaporan.dto.ts
+++ src/pengajuan/dto/tambahLaporan.dto.ts
@@ -5,10 +5,7 @@ export class LaporanDto {
   @IsNotEmpty()
   @IsInt()
   id_pengajuan: string;
-
-  @IsNotEmpty()
-  @Matches('pdf')
-  @Max(10 * 1024 * 1024, { message: 'Ukuran file tidak boleh lebih dari 10MB.' })
+  
   laporan: string;
 
 }
